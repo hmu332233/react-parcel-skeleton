758c7b0fd9cfcf62027863569c04823243f9d19d
minung.han <hmu332233@gmail.com>
2018-03-24 19:55:22 +0000

    [Init] set parcel, babel, postcss, react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.blue}>Hello world!</div>
+    )
+  } 
+}
+
+export default App;
